9e0a35c41bf50db2598902c3a61f12294d5c7f4f
Juffaz <38725561+juffaz@users.noreply.github.com>
2024-01-04 18:24:08 +0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 ;; project.clj
-(defproject clj-auth-cache/auth-proxy "0.1.0-SNAPSHOT"
+(defproject clj-auth-cache/clojure-auth-proxy "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/core.cache "0.7.1"]
                  [clj-http "3.11.0"]
