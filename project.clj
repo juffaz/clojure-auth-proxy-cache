ed0a7a5e1ced7fb337bbc67765e7cc1e48a8464c
Juffaz <38725561+juffaz@users.noreply.github.com>
2024-01-04 15:54:21 +0400

    Create project.clj

---
+++ project.clj
@@ -0,0 +1,7 @@
+;; project.clj
+(defproject your-namespace/clojure-auth-proxy "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/core.cache "0.7.1"]
+                 [clj-http "3.11.0"]
+                 [org.clojure/tools.logging "1.1.0"]]
+  :main your-namespace.auth-proxy)
