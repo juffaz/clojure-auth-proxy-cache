095ada718c6fd9826b5d484795a8a630015da5dc
Juffaz <38725561+juffaz@users.noreply.github.com>
2024-01-05 22:44:13 +0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
                  [ring/ring-jetty-adapter "1.11.0"]
                  [clj-http "3.12.0"]
                  [org.clojure/tools.logging "0.4.1"]
+                 [ch.qos.logback/logback-classic "1.2.3"]
                  [cheshire "5.10.0"]
                  [org.clojure/core.cache "1.0.217"]]
   :main ^:skip-aot clj-auth-cache.auth-proxy          
