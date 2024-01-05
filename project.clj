93e649db57abced5297f763dfc423ef352c7b63c
Juffaz <38725561+juffaz@users.noreply.github.com>
2024-01-05 14:23:08 +0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,14 @@
-;; project.clj
-(defproject clj-auth-cache/clojure-auth-proxy "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/core.cache "0.7.1"]
-                 [clj-http "3.11.0"]
-                 [org.clojure/tools.logging "1.1.0"]]
-  :main clj-auth-cache.auth-proxy)
+(defproject clj-auth-cache "0.1.0-SNAPSHOT"
+  :description "Clojure Auth Cache Proxy"
+  :dependencies [[org.clojure/clojure "1.10.4"]
+                 [ring/ring-core "1.11.0"]
+                 [ring/ring-jetty-adapter "1.11.0"]
+                 [clj-http "3.12.0"]
+                 [org.clojure/tools.logging "0.4.1"]
+                 [cheshire "5.10.0"]
+                 [org.clojure/core.cache "0.14.0"]]
+  :main ^:skip-aot clj-auth-cache.auth-proxy
+  :target-path "target/%s"
+  :profiles
+  {:uberjar {:aot :all
+             :omit-source true}})
