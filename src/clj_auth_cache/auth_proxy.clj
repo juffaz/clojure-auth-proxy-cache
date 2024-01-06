d43401da5fa3c3d107f1dfe8571980a246631cb6
Juffaz <38725561+juffaz@users.noreply.github.com>
2024-01-06 18:10:57 +0400

    Update auth_proxy.clj

--- src/clj_auth_cache/auth_proxy.clj
+++ src/clj_auth_cache/auth_proxy.clj
@@ -13,42 +13,38 @@
 (def data-cache (atom (cache/ttl-cache-factory {:ttl 3600})))
 
 (defn get-auth-token [username password auth-url]
-  (let [url (str auth-url "/userauth")
-        body (json/generate-string {:userName username :password password})
-        response (http/post url {:body body :content-type :json})]
-    response))
+  (http/post (str auth-url "/userauth")
+             {:body (json/generate-string {:userName username :password password})
+              :content-type :json}))
 
-(defn add-cache-headers [response]
-  (update response :headers assoc
-          "Content-Type" "application/json
-          "Cache-Name" "Clojure"
-          "Cache-Control" "public, max-age=3600"))
+(defn error-response
+  [message]
+  {:status 400 :body message})
 
 ;; Основная функция для обработки HTTP-запросов
 (defn app [request]
   (let [params (:body request)
         username (get params "userName")
         password (get params "password")
-        auth-url (or (System/getenv "AUTH_SERVICE_URL") "http://apigw.service.test-consul/gni/auth")]
-        (log/info (str "Received request with raw parameters: " params))
-        (log/info (str "Extracted username: " username " and password: " password))
-        (log/info (str "Auth service URL: " auth-url))
+        auth-url (or (System/getenv "AUTH_SERVICE_URL") "http://your-default-auth-service-url")]
+    (log/info (str "Received request with raw parameters: " params))
+    (log/info (str "Extracted username: " username " and password: " password))
+    (log/info (str "Auth service URL: " auth-url))
     (if (and username password)
-      (let [token-response (get-auth-token username password auth-url)]
+      (let [cached-token-response (get @data-cache username)
+            token-response (or cached-token-response
+                               (get-auth-token username password auth-url))]
         (if (= 200 (:status token-response))
-          (let [body-json (json/parse-string (:body token-response) true)
-                cached-data {:clientIdentifier (:clientIdentifier body-json)
-                             :userName (:userName body-json)
-                             :userBranch (:userBranch body-json)
-                             :systemDate (:systemDate body-json)}]
-            (swap! data-cache assoc username cached-data)
-            (add-cache-headers {:status 200
-                                :headers {"Content-Type" "application/json"}
-                                :body (:body token-response)}))
-          (add-cache-headers {:status 400
-                              :body "Authentication failed. Invalid username or password."})))
-      (add-cache-headers {:status 400
-                          :body "Bad Request. Provide both username and password."}))))
+          (do
+            (when-not cached-token-response
+              (swap! data-cache assoc username token-response))
+            {:status 200 :headers {"Content-Type" "application/json"
+                                   "Cache-Control" "public, max-age=3600"}
+             :body (:body token-response)})
+          (error-response
+            "Authentication failed. Invalid username or password.")))
+      (error-response
+        "Bad Request. Provide both username and password."))))
 
 (def handler (wrap-json-body app))
 
