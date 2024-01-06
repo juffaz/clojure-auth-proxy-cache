c371dfc125aa3db617cd858cf1965a9e43d391d3
Juffaz <38725561+juffaz@users.noreply.github.com>
2024-01-06 12:21:53 +0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [ring/ring-core "1.11.0"]
                  [ring/ring-jetty-adapter "1.11.0"]
+                 [ring/ring-json "0.5.1"]
                  [clj-http "3.12.0"]
                  [org.clojure/tools.logging "0.4.1"]
                  [ch.qos.logback/logback-classic "1.2.3"]
